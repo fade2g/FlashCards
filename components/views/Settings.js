bf19f8849eec78a53d3cd6c686b78ea8a9d6393c
fade2g <holger.heymanns@gmail.com>
2017-10-22 01:03:14 +0200

    feature: Add setting for local notifications

--- components/views/Settings.js
+++ components/views/Settings.js
@@ -1,25 +1,75 @@
 import React, {Component} from 'react';
-import {View} from 'react-native'
+import {Switch, Text, View} from 'react-native'
 import {CoreStyles} from "../../helper/CoreStyles";
+import {StyleSheet} from 'react-native';
 import {connect} from "react-redux";
 import TouchableButton from "../TouchableButton";
 import {DEFAULT_DATA} from "../../helper/defaultData";
 import {updateDeck} from "../../helper/api";
+import {
+  clearLocationNotification, getLocalNotificationMode, setLocalNotification,
+  setLocalNotificationMode
+} from "../../helper/notification";
 
 class Settings extends Component {
+
+  state = {
+    ready: false,
+    localNotification: true
+  };
+
+  componentDidMount() {
+    getLocalNotificationMode()
+      .then((mode) => {
+        this.setState({
+          ready: true,
+          localNotification: mode
+        })
+      })
+  }
+
+  handleNNotificationChange = (value) => {
+    this.setState((state) => ({
+      localNotification: value
+    }));
+    setLocalNotificationMode(value);
+    if (value) {
+      setLocalNotification();
+    } else {
+      clearLocationNotification();
+    }
+  };
+
 
   generateDefaultData = () => {
     updateDeck(DEFAULT_DATA.items, DEFAULT_DATA.deckName);
   };
 
   render() {
+    const {localNotification, ready} = this.state;
     return (<View>
-        <TouchableButton style={CoreStyles.primaryButton} onPress={this.generateDefaultData}>Add Default Data</TouchableButton>
-        <TouchableButton style={CoreStyles.primaryButton} onPress={() => this.props.navigation.goBack()}>To Home</TouchableButton>
+        <Text style={CoreStyles.text}>Settings</Text>
+        <View style={styles.switchContainer}>
+          <Text style={CoreStyles.textMinor}>Local notifications</Text>
+          {ready && <Switch onValueChange={this.handleNNotificationChange} value={localNotification}/>}
+        </View>
+        <TouchableButton style={CoreStyles.primaryButton} onPress={this.generateDefaultData}>Add Default
+          Data</TouchableButton>
+        <TouchableButton style={CoreStyles.primaryButton} onPress={() => this.props.navigation.goBack()}>To
+          Home</TouchableButton>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  switchContainer: {
+    flexDirection: 'row',
+    alignSelf: 'center'
+    // justifyContent: 'space-between'
+  }
+});
+
 
 function mapStateToProps() {
   return {}
