4eadc915c47762d6427580d475964f94812c9b08
fade2g <holger.heymanns@gmail.com>
2017-10-22 11:43:17 +0200

    refactor: Move store out f app.js into separate file
    Resolve: #13

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
 import React, {Component} from 'react';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
 import {View} from 'react-native';
 import FlashStatusBar from "./components/FlashStatusBar";
 import {primary_light} from "./helper/colors";
-import rootReducer from './reducers/rootReducer'
 import {CoreStyles} from "./helper/CoreStyles";
 import DrawerNavigation from "./components/navigation/DrawerNavigation";
+import store from "./store/index";
 
 export default class App extends Component {
 
   render() {
     return (
-      <Provider store={createStore(rootReducer)}>
+      <Provider store={store}>
         <View style={CoreStyles.flex}>
           <FlashStatusBar backgroundColor={primary_light}/>
           <DrawerNavigation/>
